910e5c02a1ae3761fe21dfb84eeea54ab633c80d
Yen Tran <yentran@Yens-MBP.lan>
2025-02-24 12:48:13 -1000

    description fix

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 export default function VanDetail(){
   const { id } = useParams();
-  const [van, setVan] = useState({})
+  const [van, setVan] = useState(null)
 
   useEffect(() => {
     fetch(`/api/vans/${id}`)
@@ -21,7 +21,7 @@ export default function VanDetail(){
           <i className={`van-type ${van.type} selected` }>{van.type}</i>
           <h2>{van.name}</h2>
           <p className='van-price'><span>${van.price}</span>/day</p>
-          <p>{van.desciption}</p>
+          <p>{van.description}</p>
           <button className='link-button'>Rent this van</button>
         </div>
       ) : <h2>Loading...</h2>}
