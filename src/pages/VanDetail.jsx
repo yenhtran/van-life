b7c0a9d0b49fe5bf15fa5b10b819faf095aa65fb
Yen Tran <yentran@Yens-MBP.lan>
2025-02-24 12:46:21 -1000

    conditional rendering

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -15,14 +15,16 @@ export default function VanDetail(){
 
   return (
     <div className='van-details-container'>
-      <div className='van-detail'>
-        <img src={van.imageUrl}/>
-        <i className={`van-type ${van.type} selected` }>{van.type}</i>
-        <h2>{van.name}</h2>
-        <p className='van-price'><span>${van.price}</span>/day</p>
-        <p>{van.desciption}</p>
-        <button className='link-button'>Rent this van</button>
-      </div>
+      {van ? (
+        <div className='van-detail'>
+          <img src={van.imageUrl}/>
+          <i className={`van-type ${van.type} selected` }>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className='van-price'><span>${van.price}</span>/day</p>
+          <p>{van.desciption}</p>
+          <button className='link-button'>Rent this van</button>
+        </div>
+      ) : <h2>Loading...</h2>}
     </div>
   )
 }
