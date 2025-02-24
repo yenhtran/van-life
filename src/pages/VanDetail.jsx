9d2ee1b5c3c65c45a3f2b810218155d28f2c652b
Yen Tran <yentran@Yens-MBP.lan>
2025-02-24 12:43:14 -1000

    add VanDetail page

---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function VanDetail(){
+  const { id } = useParams();
+  const [van, setVan] = useState({})
+
+  useEffect(() => {
+    fetch(`/api/vans/${id}`)
+      .then(res => res.json())
+      .then(data => setVan(data.vans))
+  }, [id])
+
+  console.log(van)
+
+  return (
+    <div className='van-details-container'>
+      <div className='van-detail'>
+        <img src={van.imageUrl}/>
+        <i className={`van-type ${van.type} selected` }>{van.type}</i>
+        <h2>{van.name}</h2>
+        <p className='van-price'><span>${van.price}</span>/day</p>
+        <p>{van.desciption}</p>
+        <button className='link-button'>Rent this van</button>
+      </div>
+    </div>
+  )
+}
