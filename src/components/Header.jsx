8148ef8b66fadcf5cfc3a8d1f9e7e80180b78dff
Yen Tran <yentran@Yens-MBP.lan>
2025-02-24 21:55:09 -1000

    nesting routes into Layout with Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header>
+      <Link className="site-logo" to="/">#VANLIFE</Link>
+      <nav>
+        <Link to='/about'>About</Link>
+        <Link to='/vans'>Vans</Link>
+      </nav>
+    </header>
+  )
+}
