9873353a1ab39c8318a6a25e089cee028bdad899
Yen Tran <yentran@Yens-MBP.lan>
2025-02-25 16:01:51 -1000

    nesting Host routes

--- src/index.jsx
+++ src/index.jsx
@@ -5,26 +5,29 @@ import About from './pages/About';
 import Vans from './pages/Vans/Vans';
 import VanDetail from './pages/Vans/VanDetail';
 import Layout from './components/Layout';
-import Dashboard from './pages/Host/Dashboard'
-import Income from './pages/Host/Income'
-import Reviews from './pages/Host/Reviews'
+import Dashboard from './pages/Host/Dashboard';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
+import HostLayout from './components/HostLayout';
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />}/>
-        <Route path="/host" element={<Dashboard />} />
-        <Route path="host/income" element={<Income />} />
-        <Route path="host/reviews" element={<Reviews />} />
-        <Route path="/about" element={<About />}/>
-        <Route path="/vans" element={<Vans />}/>
-        <Route path="/vans/:id" element={<VanDetail />}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="vans" element={<Vans />}/>
+          <Route path="vans/:id" element={<VanDetail />}/>
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,5 +1,10 @@
+import { Outlet } from 'react-router-dom'
+
 export default function Dashboard() {
   return (
+    <>
       <h1>Host stuff here...</h1>
+      <Outlet />
+    </>
   )
 }
