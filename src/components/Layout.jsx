0054e60a415c0f8961e0090aaded310460226405
Yen Tran <yentran@Yens-MBP.lan>
2025-02-25 23:12:49 -1000

    adds Footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout() {
   return (
-    <>
+    <div className="site-wrapper">
       <Header />
-      <Outlet />
-    </>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   )
 }
