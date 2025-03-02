7ac95c2cd94a810fbc752c535a90821c9ef7352e
Yen Tran <yentran@Yens-MBP.lan>
2025-03-01 22:13:58 -1000

    add Host Van Details

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
 import HostVans from './pages/Host/HostVans';
-import HostVanDetails from './pages/Host/HostVanDetails';
+import HostVanDetails from './pages/Host/HostVanDetail';
 import HostLayout from './components/HostLayout';
 import "./server"
 
