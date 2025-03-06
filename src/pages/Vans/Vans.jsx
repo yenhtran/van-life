368d728ec823ed3ae2580d3b00d543acfc024493
Yen Tran <yentran@Yens-MBP.lan>
2025-03-06 12:59:14 -1000

    improved vans filtering

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -29,15 +29,37 @@ export default function Vans() {
     </div>
 ))
 
+function handleFilterChange(key, value) {
+  setSearchParams(prevParams => {
+    if (value === null) {
+      prevParams.delete(key)
+    } else {
+      prevParams.set(key, value)
+    }
+    return prevParams
+  })
+}
+
   return (
       <div className="van-list-container">
         <h1>Explore our van options</h1>
         <div className="van-list-filter-buttons">
-                <button onClick={() => setSearchParams({ type: 'simple'})} className='van-type simple'>Simple</button>
-                <button onClick={() => setSearchParams({ type: 'rugged'})} className='van-type rugged'>Rugged</button>
-                <button onClick={() => setSearchParams({ type: 'luxury'})} className='van-type luxury'>Luxury</button>
-                <button onClick={() => setSearchParams({})} className='van-type clear-filters'>Clear Filters</button>
-            </div>
+          <button onClick={() => handleFilterChange('type', 'simple')} className={`van-type simple ${typeFilter === 'simple' ? 'selected' : ''}`}>
+            Simple
+          </button>
+          <button onClick={() => handleFilterChange('type', 'rugged')} className={`van-type luxury ${typeFilter === 'rugged' ? 'selected' : ''}`}>
+            Rugged
+          </button>
+          <button onClick={() => handleFilterChange('type', 'luxury')} className={`van-type rugged ${typeFilter === 'luxury' ? 'selected' : ''}`}>
+            Luxury
+          </button>
+          {
+            typeFilter ?
+            (
+              <button onClick={() => handleFilterChange('type', null)} className='van-type clear-filters'>Clear Filters</button>
+            ) : null
+          }
+        </div>
         <div className="van-list">
           {vanElements}
         </div>
