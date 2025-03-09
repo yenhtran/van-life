5fe77b48affad4e74b77e3b16b6d437ab5d26ef6
Yen Tran <yentran@Yens-MBP.lan>
2025-03-08 20:09:30 -1000

    moves getVans into api file

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useSearchParams } from 'react-router-dom'
+import { getVans } from "../../../api";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
@@ -7,9 +8,10 @@ export default function Vans() {
   const typeFilter = searchParams.get('type');
 
   useEffect(() => {
-      fetch("/api/vans")
-          .then(res => res.json())
-          .then(data => setVans(data.vans))
+    (async () => {
+      const data = await getVans();
+      setVans(data)
+    })()
   }, [])
 
   const displayedVans = typeFilter ?
