adf9b6666139d96ce3ca25f0c51c3d76b32bbfb2
Yen Tran <yentran@Yens-MBP.lan>
2025-04-21 21:28:11 -1000

    getVan now uses Firebase

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMjXMPgaDbldQOh7-g6vl_dbgxef4P2m0",
@@ -24,19 +24,14 @@ export async function getVans() {
   return vans
 }
 
-// export async function getVans(id) {
-//   const url = id ? `/api/vans/${id}` : "/api/vans"
-//   const res = await fetch(url);
-//   if (!res.ok) {
-//     throw {
-//       message: 'Failed to fetch vans',
-//       statusText: res.statusText,
-//       status: res.status
-//     }
-//   }
-//   const data = await res.json();
-//   return data.vans
-// }
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const snapshot = await getDoc(docRef);
+  return {
+    ...snapshot.data(),
+    id: snapshot.id
+  }
+}
 
 export async function getHostVans(id) {
   const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link, useLocation } from 'react-router-dom';
-import { getVans } from '../../api'
+import { getVan } from '../../api'
 
 export default function VanDetail(){
   const [van, setVan] = useState(null)
@@ -13,7 +13,7 @@ export default function VanDetail(){
     (async () => {
       setLoading(true)
       try {
-        const data = await getVans(id)
+        const data = await getVan(id)
         setVan(data)
       } catch (err) {
         setError(err)
